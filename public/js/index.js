582ddad70fe08b2d60f53b098d931c87042671bb
Dolson <D.olson4295@yahoo.com>
2019-03-05 21:01:05 -0500

    Initial Login Functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,34 @@ $(document).ready(function() {
 
   $("#login").on("click",function(){
     var username = $("#username").val().trim();
-    console.log(username);
+    var password = $("#password").val().trim();
+   
+    //console.log(username);
     $.ajax({
       method: "GET",
       url: "/api/login/" + username
     }).then(function(result)
     {
-      console.log(result);
-      console.log(result.Admin);
+      if(!result)
+      {
+        console.log("Username Does not exist");
+        $("#password").val(' ');
+        $("#username").val(' ');
+      }
+      else
+      {
+        console.log(result.Password);
+        console.log(password);
+        if(password === result.Password)
+        {
+          console.log("Login Success!");
+        }
+        else
+        {
+          console.log("Password Incorrect!");
+          $("#password").val(' ');
+        }
+    }
     });
   });
 
